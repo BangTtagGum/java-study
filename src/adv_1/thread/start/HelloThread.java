f8fb3cea931442cd103d95347606ae03a35ce078
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-09-20 16:22:13 +0900

    adv_1 Section3_1

---
+++ src/adv_1/thread/start/HelloThread.java
@@ -0,0 +1,9 @@
+package adv_1.thread.start;
+
+public class HelloThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": run()");
+    }
+}
